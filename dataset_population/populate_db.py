6a669c3c27281c8e759d684c852623b0daaa0ad3
julia <julia.sme27@gmail.com>
2025-07-09 06:11:29 +0200

    db population script fixes and imporvments

--- dataset_population/populate_db.py
+++ dataset_population/populate_db.py
@@ -4,6 +4,8 @@ import sqlalchemy as db
 
 import sys
 
+sys.path.append('../')
+
 from config import CSV_DATA_PATH, DB_CONNECTION_URL    
 print("In module products sys.path[0], __package__ ==", sys.path[0], __package__)
 
@@ -13,7 +15,6 @@ column_name_mappings = {
         'project_statements': 'statements'
     },
     'file': {
-        'file': 'path',
         'github_link': 'github_url',
         'filename': 'name',
     },
@@ -22,6 +23,9 @@ column_name_mappings = {
         'method_hash': 'hash',
         'original_loc': 'loc',
         'method_node_count': 'boa_cfg_nodes'
+    },
+    'slice': {
+        'file': 'path',
     }
 }
 
@@ -74,27 +78,19 @@ engine = db.create_engine(DB_CONNECTION_URL) # parameter echo=True for sqlalchem
 
 metadata = db.MetaData()
 
-
-def init_db():
-    create_tables()
-    insert_data_in_db()
-    return True
-
-
 def create_tables():
     db.Table(
         'Project', metadata,
         db.Column('id', db.Integer, primary_key=True, autoincrement=True),
-        db.Column('github_link', db.String),
+        db.Column('github_url', db.String),
         db.Column('statements', db.Integer)
     )
 
     db.Table(
         'File', metadata,
         db.Column('id', db.Integer, primary_key=True, autoincrement=True),
-        db.Column('github_link', db.String),
+        db.Column('github_url', db.String),
         db.Column('name', db.String),
-        db.Column('path', db.String),
         db.Column('loc', db.Numeric),
         db.Column('project_id', db.Integer, db.ForeignKey('Project.id'))
     )
@@ -113,6 +109,7 @@ def create_tables():
     db.Table(
         'Slice', metadata,
         db.Column('id', db.Integer, primary_key=True, autoincrement=True),
+        db.Column('path', db.String),
         db.Column('metadata', db.JSON),
         db.Column('code', db.String),
         db.Column('algo_type', db.String),
@@ -146,57 +143,58 @@ def create_tables():
 def insert_data_in_db():
     with engine.begin() as connection:
         # projects
-        unique_projects = df[['project', 'project_statements']] \
+        df[['project', 'project_statements']] \
             .drop_duplicates(subset=['project']) \
-            .rename(columns={
-                'project': 'github_link',
-                'project_statements': 'statements'
-            })
+            .rename(columns=column_name_mappings['project']) \
+            .to_sql('Project', connection, if_exists='append', index=False)
 
-        unique_projects.to_sql('Project', connection, if_exists='append', index=False)
+        project_ids = pandas.read_sql("SELECT id AS project_id, github_url AS project FROM Project", connection)
 
-        project_ids = pandas.read_sql("SELECT id AS project_id, github_link AS project FROM Project", connection)
-
-        print('project IDs', project_ids)
+        print('project IDs')
+        print(project_ids)
 
         # files
-        unique_files = df[['github_link', 'filename', 'file', 'loc', 'project']].drop_duplicates(subset=['github_link', 'filename'])
+        unique_files = df[['github_link', 'filename', 'loc', 'project']].drop_duplicates(subset=['github_link', 'filename'])
 
         project_files = unique_files.merge(project_ids, on='project')
-        project_files[['github_link', 'filename', 'file', 'loc', 'project_id']] \
-            .rename(columns={
-                'filename': 'name',
-                'file': 'path'}) \
+        project_files[['github_link', 'filename', 'loc', 'project_id']] \
+            .rename(columns=column_name_mappings['file']) \
             .to_sql('File', connection, if_exists='append', index=False)
         
-        file_ids = pandas.read_sql("SELECT id AS file_id, path AS file FROM File", connection)
+        file_ids = pandas.read_sql("SELECT id AS file_id, name AS filename FROM File", connection)
 
-        print('file IDs', file_ids)
+        print('file IDs')
+        print(file_ids)
 
         # methods
-        unique_methods = df[['method', 'method_hash', 'original_loc', 'method_node_count', 'file']] # todo probably also drop duplicates but on which field?
+        unique_methods = df[['method', 'method_hash', 'original_loc', 'method_node_count', 'filename']] \
+            .drop_duplicates() # todo probably also drop duplicates but on which field?
 
-        file_methods = unique_methods.merge(file_ids, on='file')
+        file_methods = unique_methods.merge(file_ids, on='filename')
         file_methods[['method', 'method_hash', 'original_loc', 'method_node_count', 'file_id']] \
             .rename(columns=column_name_mappings['method']) \
             .to_sql('Method', connection, if_exists='append', index=False)
         
-        method_ids = pandas.read_sql("SELECT id AS method_id, name AS method FROM Method", connection)
+        method_ids = pandas.read_sql("SELECT Method.id AS method_id, Method.name AS method, File.name AS filename FROM Method JOIN File ON Method.file_id = File.id", connection)
 
         print('method IDs \n', method_ids)
 
         # slices
-        method_slices = df[slice_metadata_fields + ['method']].merge(method_ids, on='method') # auf method oder method_hash überprüfen und probably auch drop_duplicates
-        # .drop_duplicates().reset_index(drop=True)
+        method_slices = df[slice_metadata_fields + ['method', 'file', 'filename']].merge(method_ids, on=['method', 'filename']) 
 
-        print(method_slices)
         print(time.strftime("%H:%M:%S %d.%m.%Y", time.localtime()))
+        
+        method_slices['metadata'] = method_slices[slice_metadata_fields].to_dict('index')
 
-        method_slices['metadata'] = method_slices[slice_metadata_fields].to_dict()
+        print(method_slices['metadata'])
 
-        method_slices.drop(columns= slice_metadata_fields + ["method"]) \
+        method_slices.drop(columns= slice_metadata_fields + ['method', 'filename'], inplace=True)
+
+        method_slices = method_slices.map(str)
+
+        method_slices.rename(columns={'file': 'path'}) \
             .to_sql('Slice', connection, if_exists='append', index=False)
-
+        
         
         print(time.strftime("%H:%M:%S %d.%m.%Y", time.localtime()))
 
@@ -209,6 +207,6 @@ def insert_data_in_db():
 
 
 if __name__ == '__main__':
-    # test1.py executed as script
+    # executed as script
     create_tables()
     insert_data_in_db()
