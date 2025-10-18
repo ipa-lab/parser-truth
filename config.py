8534d62f4c62010db4b22d66250299c3cbedf3f4
julia <julia.sme27@gmail.com>
2025-10-18 18:37:27 +0200

    force add temp config

---
+++ config.py
@@ -0,0 +1,8 @@
+CSV_DATA_PATH = '../data/analysis_results.csv'
+
+DB_PATH = '../data/adhocparser.db'
+
+DB_CONNECTION_URL = 'sqlite:///' + DB_PATH
+
+INITIAL_USER_NAME = ''
+INITIAL_USER_PW_HASH = ''
